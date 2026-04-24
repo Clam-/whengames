dc284b76bffc59daa2cf58b524ff9be5b3e015e8
Clam- <985835+Clam-@users.noreply.github.com>
2026-04-24 17:50:50 +1000

    Start over. New prompting.

--- lib/convex.ts
+++
@@ -1,7 +0,0 @@
-import "server-only";
-
-import { ConvexHttpClient } from "convex/browser";
-
-import { env } from "@/lib/env";
-
-export const getConvexHttp = () => new ConvexHttpClient(env.convexUrl());
